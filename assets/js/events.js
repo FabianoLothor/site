25d39147f6f6203a13a4814756f388dcfbafdba6
Fabiano <fabiano.lothor@gmail.com>
2015-05-03 12:42:45 -0300

    Finalizado template inicial do site

--- assets/js/events.js
+++ assets/js/events.js
@@ -1,16 +1,53 @@
 // Listeners
+document.addEventListener("loadedSettings", function(e) {
+	_settings = e.detail;
+
+	waitingSettings();
+}, false);
+
 document.addEventListener("informationReceived", function(e) {
 	information = e.detail;
 
-	$("#full_name").html(
-		information.get("name") + " " +
-		information.get("surname") + " " +
-		information.get("last_name")
-	);
+	$("#full_name").html(information.name + " " + information.surname + " " + information.last_name);
+}, false);
+
+document.addEventListener("socialMediaReceived", function(e) {
+	socialMedia = e.detail;
+
+	if(Object.keys(socialMedia).length > 0) {
+		$("#social").removeAttr("hidden");
+
+		for (index in socialMedia) {
+			if(socialMedia[index].enable) {
+				$("#social").append("<a alt='" + socialMedia[index].name + "' href='" + socialMedia[index].url + "' target='_blank'>" + "<img id='" + index + "_icon' class='social_icon' title='" + socialMedia[index].name + "' src='assets/images/social/" + index + ".svg' />" + "</a>");
+			}
+		}
+
+		$("#social img").first().addClass("first_social_icon");
+		$("#social img").last().addClass("last_social_icon");
+
+		$("#social img").jrumble({ x : 0, y : 0, rotation : 5 });
+		$("#social img").hover( function() { $(this).trigger("startRumble"); }, function(){ $(this).trigger("stopRumble"); });
+	}
 }, false);
 
 document.addEventListener("contentReceived", function(e) {
 	content = e.detail;
+	menu = content.menu;
+	colorsUsed = 0;
 
-	$("#profession").html(content.get("profession"));
+	$("#profession").html(content.profession);
+	$("#copyright").html(content.copyright.replace("{to}", getCurrentYear() > 2015 ? " - " + getCurrentYear() : ""));
+
+	for (index in menu) {
+		if(menu[index].enable) {
+			$("#menu").append("<li class='menu_item'><a alt='" + menu[index].title + "' href='#'><div id='" + index + "' class='menu_item_block " + _settings.colors[colorsUsed] + "'><i class='fa fa-" + menu[index].icon + "'></i><strong><span class='menu_item_title'>" + menu[index].title + "</span></strong><span class='menu_item_subtitle'>" + menu[index].subtitle + "</span></div></a></li>");
+			
+			$("section#" + index + ".content").addClass(_settings.colors[colorsUsed]);
+
+			++colorsUsed;
+		}
+	}
+
+	menuLoaded();
 }, false);
--- assets/js/general.js
+++ assets/js/general.js
@@ -1,14 +1,63 @@
+//
+
+var _settings;
+
 $(document).ready(function() {
 	initialize();
 });
 
 function initialize() {
-	Parse.initialize("OfQGHeUlU7pQ7t35TGZC1R8GfCugRaWlWaKiuIh9", "ZGGGk8JKEbwKOcBY50VgOKQ4uuPTiGrjGlYtM06l");
+	getSettings();
 
 	getInformation();
+	getSocialMedia();
+}
+
+function waitingSettings() {
 	getContent();
 }
 
+//
+
 function dispatchCustomEvent(eventName, args) {
 	document.dispatchEvent(new CustomEvent(eventName, {"detail": args}));
+}
+
+function getContent() {
+	loadJSON("lang_" + _settings.default_language, "contentReceived");
+}
+
+function getCurrentYear() {
+	return (new Date()).getFullYear();
+}
+
+function getInformation() {
+	loadJSON("data_info", "informationReceived");
+}
+
+function getSettings() {
+	loadJSON("settings", "loadedSettings");
+}
+
+function getSocialMedia() {
+	loadJSON("data_social", "socialMediaReceived");
+}
+
+function loadJSON(jsonName, callEvent) {
+	$.getJSON("assets/json/" + jsonName + ".json", function(json) {
+		dispatchCustomEvent(callEvent, json);
+	});
+}
+
+function menuLoaded() {
+	$(document).on("click", ".menu_item_block:not('.active')", function() {
+		$("section.content").hide();
+
+		$(".menu_item_block").removeClass("active");
+		$(this).addClass("active");
+
+		$("section#" + $(this).attr("id") + ".content").animate({ width: "toggle" }, 1000);
+	});
+
+	$(".menu_item_block").eq(0).click();
 }
